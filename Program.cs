f0e08c1b4daeb0eb26bb4a84b689ff668430b3d5
Michael Johnston <michael.johnston6042@gmail.com>
2023-05-16 11:51:09 -0400

    Added OpenAI whisper to transcribe mp3 to txt

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using Python.Runtime;
+using System;
+
 class Program
 {
     static void Main(string[] args)
@@ -7,8 +10,34 @@ class Program
 
         var downloadVideos = new DownloadVideo();
 
-        downloadVideos.DownloadClipToFile();
+        // downloadVideos.DownloadClipToFile();
 
+        // 5/12/23 note: I finished the conversion of input URL link to output video file.
+        // 5/15/23 note: finished whisper conversion.
+        // TODO: Now, call app.py to train the compcoach AI. Ask for consent in console to train AI [Y/N] because it costs money.
         
+        // TODO: When AI is trained, allow to ask questions through console and receive response in console from the AI ip address
+
+        var pythonDllLocation = "C:\\Python310\\python310.dll";
+        Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonDllLocation);
+
+        PythonEngine.Initialize();
+
+        var mp3InputDirectory = "C:\\Users\\Michael\\source\\AIBotDocs\\mp3 audios";
+        var files = Directory.GetFiles(mp3InputDirectory, "*.mp3");
+        var transcribeOutputDirectory = "C:\\Users\\Michael\\source\\AIBotDocs\\docs";
+        string modelType = "tiny";
+        using (Py.GIL())
+        {
+            var whisper = Py.Import("whisper");
+            var model = whisper.InvokeMethod("load_model", new PyString(modelType));
+            foreach(var file in files)
+            {
+                var result = model.InvokeMethod("transcribe", new PyString(file));
+                var fileName = Path.GetFileNameWithoutExtension(file);
+                File.WriteAllText(transcribeOutputDirectory + $"\\{fileName}.txt", result["text"].As<string>());
+            }
+
+        }
     }
 }
