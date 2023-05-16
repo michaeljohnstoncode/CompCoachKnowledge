4d0964320949a9c6593652fbeba4e0b095aa64d4
Michael Johnston <michael.johnston6042@gmail.com>
2023-05-16 15:35:15 -0400

    Added video to mp3 file conversion using FFMpeg

---
+++ ConvertVideoToMp3.cs
@@ -0,0 +1,22 @@
+﻿
+
+using FFMpegCore;
+
+namespace CompCoachKnowledge
+{
+    public class ConvertVideoToMp3
+    {
+        public void ConvertToMp3(string[] filesToConvert, string outputPath)
+        {
+            string staticOutputPath = outputPath;
+            foreach(string file in filesToConvert)
+            {
+                outputPath = Path.Combine(staticOutputPath + "\\" + Path.GetFileNameWithoutExtension(file) + ".mp3");
+                FFMpeg.ExtractAudio(file, outputPath);
+                Console.WriteLine($"Converted {file} to mp3 in {outputPath}");
+            }
+             
+        }
+        
+    }
+}
